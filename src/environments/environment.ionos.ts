be5b7d2fc9927b2a5ae402850ea5aadd8107c841
Jose <jldoradomoles@gmail.es>
2026-02-07 17:31:28 +0100

    fix

--- src/environments/environment.ionos.ts
+++ src/environments/environment.ionos.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-domain.com/api', // Cambiar por tu dominio de Hostinger
+  apiUrl: 'https://epicstoria.es/api', // Cambiar por tu dominio de Hostinger
   useStaticData: false, // En Hostinger usamos el backend completo
 };
